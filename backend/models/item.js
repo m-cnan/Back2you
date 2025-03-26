5bf0366ca7a0cb4d98a14e51582dbdbd8e1d6f13
Mohammed Sinan <emmohammedsinan03@gmail.com>
2025-03-27 02:05:20 +0530

    9

---
+++ backend/models/item.js
@@ -0,0 +1,21 @@
+const pool = require("../config/db");
+
+// Function to add a lost or found item
+async function addItem(type, name, location, image, email) {
+    const query = `
+        INSERT INTO ${type}_items (type, name, location, image, email)
+        VALUES ($1, $2, $3, $4, $5) RETURNING *;
+    `;
+    const values = [type, name, location, image, email];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+}
+
+// Function to get all lost or found items
+async function getItems(type) {
+    const query = `SELECT * FROM ${type}_items ORDER BY created_at DESC;`;
+    const result = await pool.query(query);
+    return result.rows;
+}
+
+module.exports = { addItem, getItems };
